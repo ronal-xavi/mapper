460adc21eb0e093c28c25c6b22cd2eb4f5917d97
Ronal Mendoza <ronalrolando.mendozatipula@emeal.nttdata.com>
2023-01-11 09:10:49 -0500

    Realización de maper Básicos con un anidado

--- src/main/java/com/ronal/mapper/mapper/EmpleadoMapper.java
+++ src/main/java/com/ronal/mapper/mapper/EmpleadoMapper.java
@@ -1,7 +1,9 @@
 package com.ronal.mapper.mapper;
 
 import com.ronal.mapper.dao.EmpleadoDAO;
+import com.ronal.mapper.dao.SedeDAO;
 import com.ronal.mapper.dto.EmpleadoDTO;
+import com.ronal.mapper.dto.SedeDTO;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 
@@ -10,12 +12,20 @@ import org.mapstruct.Mapping;
 public interface EmpleadoMapper {
 
 
+    //TARGET SON LOS ATRIBUTOS DEL OBJETO QUE SE DEVUELVE /// SOURCE SON LOS ATRIBUTOS DEL OBJETO QUE ESTA COMO PARÁMETRO
+
     @Mapping(target = "nombre", source = "name")
     @Mapping(target = "apellido", source = "lastname")
+    @Mapping(target = "sede",source = "sedeDTO")
     EmpleadoDAO empleadoDtoToEmpleadoDAO(EmpleadoDTO empleadoDTO);
 
 
     @Mapping(target = "name", source = "nombre")
     @Mapping(target = "lastname", source = "apellido")
     EmpleadoDTO empleadoDaoToEmpleadoDTO(EmpleadoDAO empleadoDAO);
+
+
+    SedeDAO sedeDtoToSedeDao(SedeDTO sedeDTO);
+
+    SedeDTO sedeDaoToSedeDto(SedeDAO sedeDAO);
 }
